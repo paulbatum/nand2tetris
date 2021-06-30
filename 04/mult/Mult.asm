e9d6a926bac33806f85c317f9c7e64361f9bed42
Paul Batum <paul.batum@gmail.com>
2021-06-29 18:18:45 -0700

    Multiply

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,35 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    // initialize R2 to zero
+    D=0
+    @R2
+    M=D
+    // initialze n to R0
+    @R0
+    D=M
+    @n
+    M=D
+    // TODO: shortcircuit if both R0 and R1 are zero
+(LOOP)
+    // if n == 0 jump to END
+    @n
+    D=M
+    @END
+    D;JEQ
+    // R2 = R2 + R1
+    @R2
+    D=M
+    @R1
+    D=D+M
+    @R2
+    M=D
+    // n = n - 1
+    @n
+    M=M-1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
