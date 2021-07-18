7a2073d8ba852fc06d40f0530d4d48bddea60bde
Paul Batum <paul.batum@gmail.com>
2021-07-17 22:10:14 -0700

    BasicTest now passes

--- 07/VMTranslator/VMTranslator/CodeWriter.cs
+++ 07/VMTranslator/VMTranslator/CodeWriter.cs
@@ -29,13 +29,34 @@ namespace VMTranslator
                     }
                     else
                     {
-                        throw new NotImplementedException("Can only handle constants");
+                        writer.WriteLine($"@{MapSegment(segment)}");
+                        var r1 = segment == "temp" ? "A" : "M"; 
+                        writer.WriteLine($"D={r1}");
+                        writer.WriteLine($"@{index}");
+                        writer.WriteLine($"A=D+A");  // go to the target address                        
+                        writer.WriteLine("D=M"); // store the value at the address into D
+                        WritePushD();                        
                     }
                     break;
                 case CommandType.C_POP:
                     writer.WriteLine($"// pop {segment} {index}");
-                    throw new NotImplementedException("no popping yet");
-                //break;
+                    // todo - swap the order here so its more efficient
+                    WritePopD();
+                    writer.WriteLine("@R13"); // store the popped value in r13
+                    writer.WriteLine("M=D"); 
+                    writer.WriteLine($"@{MapSegment(segment)}"); // figure out the target address
+                    var r2 = segment == "temp" ? "A" : "M";
+                    writer.WriteLine($"D={r2}");
+                    writer.WriteLine($"@{index}");
+                    writer.WriteLine($"D=D+A");
+                    writer.WriteLine("@R14"); // save it in r14
+                    writer.WriteLine("M=D");
+                    writer.WriteLine("@R13"); // go back to r13 and load into D
+                    writer.WriteLine("D=M");
+                    writer.WriteLine("@R14"); // go the address saved in r14 and set it to D
+                    writer.WriteLine("A=M");
+                    writer.WriteLine("M=D");                    
+                    break;
                 default:
                     throw new ArgumentException($"Cannot push/pop command '{commandType}'.");
             }
@@ -67,6 +88,19 @@ namespace VMTranslator
                     WriteDoubleOperandArithmetic(command);
                     break;
             }            
+        }
+
+        private string MapSegment(string segment)
+        {
+            return segment switch
+            {
+                "local" => "LCL",
+                "argument" => "ARG",
+                "this" => "THIS",
+                "that" => "THAT",                
+                "temp" => "5",
+                _ => throw new ArgumentException($"Unknown segment to map: {segment}")
+            };
         }
 
         private void WriteDoubleOperandArithmetic(string command)
@@ -140,7 +174,7 @@ namespace VMTranslator
             writer.WriteLine("M=M-1");
             writer.WriteLine("A=M");
             writer.WriteLine("D=M");
-            writer.WriteLine("@SP");            
+            writer.WriteLine("@SP"); //what is this?           
         }
     }
 }
