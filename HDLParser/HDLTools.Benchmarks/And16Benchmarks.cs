e56302ae9bf8fc61c8a21e2efb49f480c92c9b2a
Paul Batum <paul.batum@gmail.com>
2022-02-15 14:34:49 -0800

    screwing around with benchmark.net

---
+++ HDLParser/HDLTools.Benchmarks/And16Benchmarks.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDLTools.Benchmarks
+{
+    public class And16Benchmarks
+    {
+        private Chip chip;
+        private Pin pinOut;
+        private int[] valueA;
+        private int[] valueB;
+
+        public And16Benchmarks()
+        {
+            var library = new ChipLibrary();
+            library.Register(HDLParser.ParseString(File.ReadAllText(@"hdl\Not.hdl")).Single());
+            library.Register(HDLParser.ParseString(File.ReadAllText(@"hdl\And.hdl")).Single());
+
+            var hdl = File.ReadAllText(@"hdl\And16.hdl");
+            ChipDescription desc = HDLParser.ParseString(hdl).Single();
+
+            chip = new Chip(desc, library);
+
+            var pinA = chip.Pins.Single(x => x.Name == "a");
+            var pinB = chip.Pins.Single(x => x.Name == "b");
+            pinOut = chip.Pins.Single(x => x.Name == "out");
+
+            valueA = Conversions.ConvertBinaryStringToIntArray("0011110011000011");
+            valueB = Conversions.ConvertBinaryStringToIntArray("0000111111110000");
+            pinA.Init(valueA);
+            pinB.Init(valueB);
+        }
+
+        [Benchmark]
+        public int[] And16Chip()
+        {
+            chip.Simulate(0);
+            var result = pinOut.GetValue(0);
+            chip.InvalidateOutputs(0);
+            return result;
+        }
+
+    }
+}
---
+++ HDLParser/HDLTools.Benchmarks/NandBenchmarks.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using HDLTools.Builtin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDLTools.Benchmarks
+{
+    public class NandBenchmarks
+    {
+        private Nand chip;
+        private Pin pinA;
+        private Pin pinB;
+        private Pin pinOut;
+
+        public NandBenchmarks()
+        {
+            chip = new Nand();
+            pinA = chip.Pins.Single(x => x.Name == "a");
+            pinB = chip.Pins.Single(x => x.Name == "b");
+            pinOut = chip.Pins.Single(x => x.Name == "out");
+
+            pinA.Init(0);
+            pinB.Init(1);
+        }
+
+        //[Benchmark]
+        public int NandGate()
+        {                        
+            chip.Simulate(0);
+            var result = pinOut.GetBit(0);
+            chip.InvalidateOutputs(0);
+            return result;
+        }
+
+        public Vector128<byte> Avx2AndNot()
+        {
+            byte byteA = 0b0000_0000;
+            byte byteB = 0b1111_1111;
+            byte byteC = 0b0101_0101;
+            byte byteD = 0b1010_1010;
+
+            var a = Vector128.Create(byteA, byteB, byteC, byteD, byteA, byteB, byteC, byteD, byteA, byteB, byteC, byteD, byteA, byteB, byteC, byteD);            
+            var b = Vector128.Create(byteA, byteB, byteC, byteD, byteA, byteB, byteC, byteD, byteA, byteB, byteC, byteD, byteA, byteB, byteC, byteD);
+
+            // unfortunately, AndNot is not a Nand operation.
+            var result = Avx2.AndNot(a, b);
+            return result;
+        }
+
+    }
+}
