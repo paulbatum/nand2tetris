d4cffe029dfaa03b4917e2cd43d6552a2f5038e2
Paul Batum <paul.batum@gmail.com>
2022-12-01 21:59:22 -0800

    jack tokenizer

--- 10/JackCompiler/JackAnalyzer/JackTokenizer.cs
+++ 10/JackCompiler/JackAnalyzer/JackTokenizer.cs
@@ -1,12 +1,162 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JackAnalyzer
 {
-    internal class JackTokenizer
+    public class JackTokenizer
     {
+        private IEnumerator<Token> tokens;
+        public JackTokenizer(StreamReader reader)
+        {
+            this.tokens = Tokenize(reader).GetEnumerator();
+            HasMoreTokens = this.tokens.MoveNext();
+        }
+
+        public Token? CurrentToken { get; private set; } = null;
+        public bool HasMoreTokens { get; private set; }
+
+        public void Advance()
+        {
+            CurrentToken = this.tokens.Current;
+            HasMoreTokens = tokens.MoveNext();
+        }
+
+        private IEnumerable<Token> Tokenize(StreamReader reader)
+        {
+            var keywords = typeof(Keyword)
+                .GetEnumNames()
+                .Select(x => x.ToLower());
+
+            var symbols = new char[] { '{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~' };
+
+            char readChar;
+            while(!reader.EndOfStream)
+            {
+                readChar = (char) reader.Read();
+
+                if (char.IsWhiteSpace(readChar))
+                    continue;
+
+                if(readChar == '/')
+                {
+                    if (reader.Peek() == '/')
+                    {
+                        reader.ReadLine();
+                        continue;
+                    }
+
+                    if (reader.Peek() == '*')
+                    {
+                        reader.Read(); // consume the *
+                        while (!reader.EndOfStream)
+                        {
+                            readChar = (char)reader.Read();
+                            if (readChar == '*' && reader.Peek() == '/')
+                            {
+                                reader.Read(); // consume the /
+                                break;
+                            }
+                        }
+                        continue;
+                    }
+                }
+
+                if(symbols.Contains(readChar))
+                {
+                    yield return new Token(TokenType.Symbol, readChar);
+                    continue;
+                }
+
+                if(char.IsDigit(readChar))
+                {
+                    string integerConstant = readChar.ToString();
+                    while(!reader.EndOfStream && char.IsDigit((char) reader.Peek()))
+                    {
+                        readChar = (char)reader.Read();
+                        integerConstant += readChar;
+                    }
+                    yield return new Token(TokenType.IntegerConstant, integerConstant);
+                    continue;
+                }
+                
+                if(char.IsLetter(readChar) || readChar == '_')
+                {
+                    bool IsIdentifierChar(char c) => char.IsLetterOrDigit(c) || c == '_';
+                    string keywordOrIdentifier = readChar.ToString();
+                    while(!reader.EndOfStream && IsIdentifierChar((char)reader.Peek()))
+                    {
+                        readChar = (char)reader.Read();
+                        keywordOrIdentifier += readChar;
+                    }
+
+                    if(keywords.Contains(keywordOrIdentifier))
+                    {
+                        yield return new Token(TokenType.Keyword, keywordOrIdentifier);
+                        continue;
+                    }
+
+                    yield return new Token(TokenType.Identifier, keywordOrIdentifier);
+                    continue;
+                }
+
+                if(readChar == '"')
+                {
+                    string stringLiteral = "";
+                    while (!reader.EndOfStream && reader.Peek() != '"')
+                    {
+                        readChar = (char)reader.Read();
+                        stringLiteral += readChar;
+                    }
+                    reader.Read(); // consume the end quote
+                    yield return new Token(TokenType.StringConstant, stringLiteral);
+                    continue;
+                }
+
+                // we shouldn't be here
+                Debug.WriteLine("error - dumping remaining input");
+                Debug.WriteLine(reader.ReadToEnd());
+                throw new Exception($"Failed to tokenize");
+            }
+        }
+    }
+
+    public record Token(TokenType TokenType, object Value);
+
+    public enum TokenType
+    {
+        Keyword,
+        Symbol,
+        Identifier,
+        IntegerConstant,
+        StringConstant
+    }
+
+    public enum Keyword
+    {
+        Class,
+        Method,
+        Function,
+        Constructor,
+        Int,
+        Boolean,
+        Char,
+        Void,
+        Var,
+        Static,
+        Field,
+        Let,
+        Do,
+        If,
+        Else,
+        While,
+        Return,
+        True,
+        False,
+        Null,
+        This
     }
 }
--- 10/JackCompiler/JackAnalyzer/Program.cs
+++ 10/JackCompiler/JackAnalyzer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace JackAnalyzer
 {
@@ -22,27 +23,67 @@ namespace JackAnalyzer
                 return;
 
             string parentDir = Directory.GetParent(files[0]).FullName;
-            var outputFileName = Path.Combine(parentDir, Path.GetFileNameWithoutExtension(parentDir) + ".xml");
 
-
-            using (var output = new StreamWriter(outputFileName))
+            foreach (var f in files)
             {
-                var codeWriter = new CodeWriter("bootstrap", output);
-                codeWriter.WriteBootstrap();
-
-                var translator = new Translator(codeWriter);
-
-                foreach (var f in files)
+                using (var input = new StreamReader(f))
                 {
-                    using (var input = new StreamReader(f))
-                    {
-                        Console.WriteLine("Reading {0}", Path.GetFullPath(f));
-                        translator.Translate(Path.GetFileNameWithoutExtension(f), input);
-                    }
-                }
+                    var outputFileName = Path.Combine(parentDir, Path.GetFileNameWithoutExtension(f) + "T.output.xml");
+                    StringBuilder output = new StringBuilder();
 
-                Console.WriteLine("Wrote {0}", Path.GetFullPath(outputFileName));
+                    JackTokenizer tokenizer = new JackTokenizer(input);
+                    output.AppendLine("<tokens>");
+                    while(tokenizer.HasMoreTokens)
+                    {
+                        tokenizer.Advance();
+
+                        Token current = tokenizer.CurrentToken;
+                        string tokenName = Enum.GetName(current.TokenType)!;
+                        tokenName = char.ToLower(tokenName[0]) + tokenName.Substring(1);
+
+                        string outVal = current.Value switch
+                        {
+                            char c when c == '>' => "&gt;",
+                            char c when c == '<' => "&lt;",
+                            char c when c == '&' => "&amp;",
+                            object o => o.ToString()
+                        };
+
+                        output.AppendLine($"<{tokenName}> {outVal} </{tokenName}>");
+
+                        
+                    }
+                    output.AppendLine("</tokens>");
+
+                    File.WriteAllText(outputFileName, output.ToString());
+                }
             }
+
+                    
+                    
+
+            //string parentDir = Directory.GetParent(files[0]).FullName;
+            //var outputFileName = Path.Combine(parentDir, Path.GetFileNameWithoutExtension(parentDir) + ".xml");
+
+
+            //using (var output = new StreamWriter(outputFileName))
+            //{
+            //    var codeWriter = new CodeWriter("bootstrap", output);
+            //    codeWriter.WriteBootstrap();
+
+            //    var translator = new Translator(codeWriter);
+
+            //    foreach (var f in files)
+            //    {
+            //        using (var input = new StreamReader(f))
+            //        {
+            //            Console.WriteLine("Reading {0}", Path.GetFullPath(f));
+            //            translator.Translate(Path.GetFileNameWithoutExtension(f), input);
+            //        }
+            //    }
+
+            //    Console.WriteLine("Wrote {0}", Path.GetFullPath(outputFileName));
+            
         }
     }
 }
